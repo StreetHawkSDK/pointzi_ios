ab85a41d41711704b4b5073d1326aa626e28630c
ChristineXYS <christine@streethawk.com>
2018-02-02 14:20:29 +0800

    Fix IOS-1247: CI: unlock keychain Remove unlock command and add into salt.

--- build.sh
+++ build.sh
@@ -33,8 +33,6 @@ echo "Build Pointzi demos"
 
 export PATH="$HOME/.fastlane/bin:$PATH"
 
-security unlock-keychain -p $BUILD_PASSWORD "/Users/hawk/Library/Keychains/login.keychain-db"
-
 # ------------------- build PZStatic ------------------------
 
 pushd .
