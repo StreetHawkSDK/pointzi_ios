2b112b50e22fa1987bd2b2f1a0233777ab3d5bd4
EthanXingyuJi <ethan@streethawk.com>
2018-07-19 17:30:56 +1000

    refine auto versioning logic

--- build.sh
+++ build.sh
@@ -84,15 +84,22 @@ if [ ! -z "$GIT_EMAIL" ]; then
     git config user.email "$GIT_EMAIL"
 fi
 if [ ! -z "$COMMIT_MESSAGE" ]; then
-    head_version=$(git tag -l --points-at HEAD | grep -v '^_')
-    if [ -z $head_version ]; then
-        release_version=$(git semver get | python -c 'import semver, sys;print(semver.bump_patch(sys.stdin.read()))')-beta+$(date +%s)
+    head_version=$(git -C $POINTZI_LIB_PATH tag -l --points-at HEAD | grep -v '^_')
+    current_version=$(git -C $POINTZI_LIB_PATH describe --tags --abbrev=0)
+    # if current recheable tag version include beta as a suffix, then do a beta release with the same version number
+    if [[ $current_version =~ ^[0-9]+\.[0-9]+\.[0-9]+-beta.*$ ]]; then
+        release_version=$([[ $current_version =~ ^([0-9]+\.[0-9]+\.[0-9]+-beta).*$ ]] && echo ${BASH_REMATCH[1]})+$(date +%s)
+    # if current recheable tag version does not include beta as a suffix but head version is empty,
+    # then bump up patch number and do a beta release
+    elif [ -z $head_version ]; then
+        release_version=$(git -C $POINTZI_LIB_PATH describe --tags --abbrev=0 | python -c 'import semver, sys;print(semver.bump_patch(sys.stdin.read()))')-beta+$(date +%s)
+    # if current recheable tag version does not include beta as a suffix and head version is not empty,
+    # then check if the version number adhere to semver x.x.x if so, do a formal release
     elif [[ $head_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         release_version=$head_version
-    elif [[ $head_version =~ ^[0-9]+\.[0-9]+\.[0-9]+-beta$ ]]; then
-        release_version=$head_version+$(date +%s)
     else
-        echo "head tag version fetched from source repo is neither a beta version nor a release version, please check!"
+        echo "head tag version fetched from source repo is neither a beta version nor release version, please check!"
+        exit 128
     fi
     sed -i .bak 's/\(s.version[[:space:]]*=[[:space:]]"\).*/\1'"$release_version"'"/g' pointzi.podspec
     git add pointzi.podspec
